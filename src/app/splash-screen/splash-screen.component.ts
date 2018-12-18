a1061c660fb0cc5a0c0ae120a1fab229b8eb0fd8
elizalc <elizabethalcala28@gmail.com>
2018-12-18 07:11:02 -0500

    Set scss

--- src/app/splash-screen/splash-screen.component.ts
+++ src/app/splash-screen/splash-screen.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ns-splash-screen',
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.css'],
   moduleId: module.id,
 })
 export class SplashScreenComponent implements OnInit {
