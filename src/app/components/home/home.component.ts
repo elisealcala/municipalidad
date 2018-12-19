85f7b2e677799da4f57132d3b4d11cd370f5abf6
elizalc <elizabethalcala28@gmail.com>
2018-12-19 08:38:37 -0500

    Add styles to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public sections = [
+    {
+      title: "Boton S.O.S",
+      description: "Botón de emergencia",
+    },
+    {
+      title: "Acerca de La Molina",
+      description: "Entérate más sobra La Molina",
+    },
+    {
+      title: "Contraloría Vecinal",
+      description: "Espacio para ver los gastos de la municipalidad",
+    },
+    {
+      title: "Quejas y Sugerencias",
+      description: "Déjanos tus"
+    },
+    {
+      title: "Datos Útiles",
+      description: "Listado de datos útiles",
+    }
+  ]
+
+  constructor() {}
 
   ngOnInit() {
   }
