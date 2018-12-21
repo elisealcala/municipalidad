ea0af27da6d46b0aa8b89db6300832b10a42ac8e
elizalc <elizabethalcala28@gmail.com>
2018-12-21 08:25:18 -0500

    Change on view

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,36 +4,73 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ns-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  moduleId: module.id,
+  moduleId: module.id
 })
 export class HomeComponent implements OnInit {
-
   public sections = [
     {
-      title: "Boton S.O.S",
-      description: "Botón de emergencia",
+      title: 'Boton S.O.S',
+      description: 'Botón de emergencia'
     },
     {
-      title: "Acerca de La Molina",
-      description: "Entérate más sobra La Molina",
+      title: 'Acerca de La Molina',
+      description: 'Entérate más sobra La Molina'
     },
     {
-      title: "Contraloría Vecinal",
-      description: "Espacio para ver los gastos de la municipalidad",
+      title: 'Contraloría Vecinal',
+      description: 'Espacio para ver los gastos de la municipalidad'
     },
     {
-      title: "Quejas y Sugerencias",
-      description: "Déjanos tus"
+      title: 'Quejas y Sugerencias',
+      description: 'Déjanos tus'
     },
     {
-      title: "Datos Útiles",
-      description: "Listado de datos útiles",
+      title: 'Datos Útiles',
+      description: 'Listado de datos útiles'
+    },
+    {
+      title: 'Boton S.O.S',
+      description: 'Botón de emergencia'
+    },
+    {
+      title: 'Acerca de La Molina',
+      description: 'Entérate más sobra La Molina'
+    },
+    {
+      title: 'Contraloría Vecinal',
+      description: 'Espacio para ver los gastos de la municipalidad'
+    },
+    {
+      title: 'Quejas y Sugerencias',
+      description: 'Déjanos tus'
+    },
+    {
+      title: 'Datos Útiles',
+      description: 'Listado de datos útiles'
+    },
+    {
+      title: 'Boton S.O.S',
+      description: 'Botón de emergencia'
+    },
+    {
+      title: 'Acerca de La Molina',
+      description: 'Entérate más sobra La Molina'
+    },
+    {
+      title: 'Contraloría Vecinal',
+      description: 'Espacio para ver los gastos de la municipalidad'
+    },
+    {
+      title: 'Quejas y Sugerencias',
+      description: 'Déjanos tus'
+    },
+    {
+      title: 'Datos Útiles',
+      description: 'Listado de datos útiles'
     }
-  ]
+  ];
 
   constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
