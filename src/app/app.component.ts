169b663118b3da7a161992639e2da628223e3a22
elizalc <elizabethalcala28@gmail.com>
2019-01-22 01:01:28 -0500

    Add animation on informes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { CardView } from 'nativescript-cardview';
+import { registerElement } from 'nativescript-angular/element-registry';
 
 @Component({
-    moduleId: module.id,
-    selector: "ns-app",
-    templateUrl: "app.component.html"
+  moduleId: module.id,
+  selector: 'ns-app',
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+    registerElement('CardView', () => CardView);
+  }
 }
--- src/app/components/informante/informante.component.ts
+++ src/app/components/informante/informante.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from 'tns-core-modules/ui/page/page';
 import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
-import { registerElement } from 'nativescript-angular/element-registry';
+// import { registerElement } from 'nativescript-angular/element-registry';
 import { RouterExtensions } from 'nativescript-angular/router';
-import { CardView } from 'nativescript-cardview';
-import { ScrollEventData } from "tns-core-modules/ui/scroll-view";
+// import { CardView } from 'nativescript-cardview';
+import { ScrollEventData } from 'tns-core-modules/ui/scroll-view';
 import { defer, interval, animationFrameScheduler } from 'rxjs';
 import { map, takeWhile } from 'rxjs/operators';
 
 const timeElapsed = defer(() => {
   const start = animationFrameScheduler.now();
-  return interval(1).pipe(map(() => Math.floor((Date.now() - start))));
+  return interval(1).pipe(map(() => Math.floor(Date.now() - start)));
 });
 
-export const durationForAnimation = (totalMs) => timeElapsed.pipe(map((elapsedMs: number) => elapsedMs / totalMs), takeWhile(t => t <= 1));
-export const amount = (d) => (t) => t * d;
-export const elasticOut = (t) => Math.sin(-13.0 * (t + 1.0) * Math.PI / 2) * Math.pow(2.0, -10.0 * t) + 1.0;
+const durationForAnimation = totalMs =>
+  timeElapsed.pipe(
+    map((elapsedMs: number) => elapsedMs / totalMs),
+    takeWhile(t => t <= 1),
+  );
+const amount = (d, h) => t => {
+  if (d * ((h / d) * t) < h - d) {
+    return h - d * ((h / d) * t);
+  }
+};
+// const elasticOut = t => Math.sin((-13.0 * (t + 1.0) * Math.PI) / 2) * Math.pow(2.0, -10.0 * t) + 1.0;
 
 @Component({
   selector: 'ns-informante',
@@ -24,7 +32,6 @@ export const elasticOut = (t) => Math.sin(-13.0 * (t + 1.0) * Math.PI / 2) * Mat
   moduleId: module.id,
 })
 export class InformanteComponent implements OnInit {
-
   public reports = [
     {
       title: 'Limpieza Pública',
@@ -57,42 +64,39 @@ export class InformanteComponent implements OnInit {
     {
       title: 'Iluminación',
       icon: '\uf66f',
-    }
+    },
   ];
 
-  public status = "not scrolling";
-
+  public status = 'not scrolling';
+  public isHide = false;
 
   constructor(page: Page, private router: RouterExtensions) {
     page.actionBarHidden = true;
   }
 
-  ngOnInit() {
-    registerElement('CardView', () => CardView);
-  }
-
-  public onTap(lbl: StackLayout) {
-    durationForAnimation(2000).pipe(map(elasticOut), map(amount(50))).subscribe((heightValue) => {
-      lbl.style.height = heightValue;
-    });
+  public ngOnInit() {
+    // registerElement('CardView', () => CardView);
   }
 
   public onScroll(args: ScrollEventData, lbl: StackLayout) {
-    this.status = "scrolling";
+    this.status = 'scrolling';
 
     setTimeout(() => {
-      this.status = "not scrolling";
+      this.status = 'not scrolling';
     }, 300);
 
-    if (args.scrollY > 0 && args.scrollY < 125) {
-      durationForAnimation(2000).pipe(map(elasticOut), map(amount(50))).subscribe((heightValue) => {
-        lbl.style.height = heightValue;
-      });
+    if (args.scrollY > 0 && args.scrollY < 125 && !this.isHide) {
+      durationForAnimation(500)
+        .pipe(
+          // map(elasticOut),
+          map(amount(56, 170)),
+        )
+        .subscribe(heightValue => {
+          lbl.style.height = heightValue;
+          this.isHide = true;
+        });
     }
-
-    console.log("scrollY: " + args.scrollY);
   }
-
 
   public goBack() {
     this.router.back();
